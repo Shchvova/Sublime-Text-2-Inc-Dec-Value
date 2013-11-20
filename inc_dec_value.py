f84ff4d73488997c49d23e5510435b6b097401f4
Svoka <shchvova@gmail.com>
2013-11-20 11:51:15 -0500

    fixing multiple selection problems

--- inc_dec_value.py
+++ inc_dec_value.py
@@ -298,7 +298,7 @@ class IncDecValueCommand(sublime_plugin.TextCommand):
             self.replace(str(result) + match2, tmp_reg)
 
             pos = self.view.sel()[self.region_index]
-            if orig_int >= 0 and result < 0 and pos.a == tmp_reg.a:
+            if int(orig_int) >= 0 and result < 0 and pos.a == tmp_reg.a:
                 self.view.sel().subtract(tmp_reg)
                 self.view.sel().add(sublime.Region(tmp_reg.a+1, tmp_reg.a+1))
 
